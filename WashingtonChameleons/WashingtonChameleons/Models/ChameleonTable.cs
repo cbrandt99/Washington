34328c0fd29d1ca38646b99f2f01d770c7200e74
Joey Pekar <47425421+JoeyPekar@users.noreply.github.com>
2020-03-19 16:52:40 -0400

    The Clean
    Recreated Clean Project
    Organized imported Libraries
    Implemented SCSS Style Sheet Compiler
    Adjusted MapQuest data
    Began CSS Components

---
+++ WashingtonChameleons/WashingtonChameleons/Models/ChameleonTable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WashingtonChameleons.Models
+{
+    public partial class ChameleonTable
+    {
+        public string CurrentName { get; set; }
+        public string FormerName { get; set; }
+        public string ContactNumber { get; set; }
+        public string ContactEmail { get; set; }
+        public DateTime? DateRegistered { get; set; }
+        public DateTime? DateOoS { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+        public int? ConfidenceLevel { get; set; }
+        public int ChameleonId { get; set; }
+    }
+}
